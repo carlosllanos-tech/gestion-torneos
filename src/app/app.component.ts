fcf1308340a50d0b631a9e5483a019c075160446
CarlosLlanosRodriguez <carlosraiton@gmail.com>
2025-11-18 22:15:07 -0400

    Inicio de proyecto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    CommonModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'gestion-torneos';
+  title = 'Hola gestion-torneos';
+
+  isLogged: boolean = false;
+
+  nombres = ['Juan', 'Beto', 'Pedro'];
+
+  rol = 'admin111111';
+
 }
